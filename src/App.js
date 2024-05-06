2bb7aceea6ef2c1379190c2de13f139048158cb8
Malek Aozon <malekozon123@gmail.com>
2024-05-06 11:53:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="image">
+      <img src="../images/dd.jpg"  />
+      </div>
+      <div className="name" >
+        <h1>Obaida Nakkar</h1>
+        <h3>Python Developper</h3>
+        <h5>Dahab جداً</h5>
+      </div>
+      <div className="box">
+        <div className="email">
+          <img src="../images/gm.svg"/>
+          <h3>Email</h3>
+        </div>
+        <div className="Linkedin">
+          <img src="../images/li.svg" />
+          <h3>Linkedin</h3>
+        </div>
+      </div>
+      <div className="about">
+        <h2>About</h2>
+        <p>
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        </p>
+      </div>
+      <div className="inte">
+        <h2>interest</h2>
+        <p>
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        lorem lorem lorem lorem lorem lorem lorem
+        </p>
+      </div>
+      <div className="footer">
+        <img src="../images/li.svg" />
+        <img src="../images/li.svg" />
+        <img src="../images/li.svg" />
+        <img src="../images/li.svg" />
+      </div>
+    </div>
+  );
+}
+
